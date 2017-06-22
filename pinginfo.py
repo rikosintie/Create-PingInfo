5129af139dab5e340dd0cdcb753bb24378e402d8
Michael Hubbard <michael.hubbard999@gmail.com>
2017-06-21 22:36:38 -0700

    Add files via upload
    Added an input for one IP address per switch or two IP address per switch.

--- pinginfo.py
+++ pinginfo.py
@@ -1,6 +1,16 @@
 '''
 read a file 'pinginfo.txt' containing the output of sh cdp ne det | i Dev|IP a
-It should look like this:
+It should look like this (enter 1 when prompted):
+
+Device ID: test-MDF1
+  IP address: 10.52.1.10
+Device ID: test-MDF1
+  IP address: 10.52.1.10
+Device ID: test_IDF2_Conf
+  IP address: 10.52.1.30
+
+or this (enter 2 when prompted)
+
 Device ID: SBHS-IDFL-LDAT-SW01
   IP address: 10.131.3.116
   IP address: 10.131.3.116
@@ -24,6 +34,19 @@ f = open('pinginfo.txt', 'r')
 for line in f:
     listname.append(line)
 f.close
+#remove blank line at end of list
+while True:
+  try:
+    listname.remove('\n')
+  except ValueError:
+    break
+items = len(listname)
+
+#some switches output two "IP address" lines and some output one.
+#enter the number of IP address lines per switch
+numberofIPs = int(input('Enter the number of IP addresses per switch 1 or 2: '))
+numberofIPs = numberofIPs + 1
+
 #Build the end condition for the while loop.
 items = len(listname)-1
 #initalize the loop counter
@@ -49,11 +72,11 @@ while counter < items:
     IPaddress = IPaddress.replace(':','')
     IPaddress = IPaddress.strip('\n')
     #print the information needed to create the interface description
-    temp = IPaddress + ' ' + hostname
+    temp = IPaddress + hostname
 #    print(temp)
     sItems.append(temp)
 #increment the counter by three to jump to the next hostname line
-    counter = counter + 3
+    counter = counter + numberofIPs
 print()
 sItems.sort()
 for s in sItems:
